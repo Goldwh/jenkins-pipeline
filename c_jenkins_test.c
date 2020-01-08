9b05c30ce02a4a6bac5268381d4878bd95fd2788
Goldwh <794918503@qq.com>
2020-01-08 19:42:42 +0800

    the c_jenkins_test.c is changed

--- c_jenkins_test.c
+++ c_jenkins_test.c
@@ -5,10 +5,15 @@ int main()
     int i = 10;
     printf("Hello world\n");
     printf("thank you\n");
-    while(i--)
-	{
-	    printf("**");	
-	    printf("i = %d\n",i);
-	}
+    for(int j=0; j<=10; j++)
+    {
+	printf("第%d次循环：\n",j);
+    	while(i--)
+    	    {
+	        printf("**");	
+	        printf("i = %d\n",i);
+	    }
+	i = 10;
+    }
     exit(0);
 }
